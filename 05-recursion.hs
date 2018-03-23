3bb23036b87c8e060b81a805cd4ed90d2505bb18
something <nnebp@users.noreply.github.com>
2018-03-23 08:24:57 -0400

    04

--- 05-recursion.hs
+++ 05-recursion.hs
@@ -1,7 +1,9 @@
 -- Raise x to the power y, using recursion
 -- For example, power 5 2 = 25
 power :: Int -> Int -> Int
-power x y = undefined
+power _ 0 = 1
+power x 1 = x 
+power x y = x * power x (y - 1)
 
 -- create a list of length n of the fibbonaci sequence in reverse order
 -- examples: fib 0 = [0]
@@ -9,7 +11,7 @@ power x y = undefined
 --	     fib 10 = [55,34,21,13,8,5,3,2,1,1,0]	
 -- try to use a where clause
 fib :: (Num a, Eq a) => a -> [a]
-fib x = undefined
+fib x = 
 
 -- This is not recursive, but have a go anyway.
 -- Create a function which takes two parameters, a number and a step
